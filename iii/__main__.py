691037cf310fe899ea8ea0a6c2b04013c63991f5
frnyb <ffn@mmmi.sdu.dk>
2024-06-03 08:43:20 +0000

    Added help to sub commands

--- iii/__main__.py
+++ iii/__main__.py
@@ -6,10 +6,10 @@ def main():
     parser = argparse.ArgumentParser(prog='iii')
     subparsers = parser.add_subparsers(dest='subcommand')
 
-    parser_system = subparsers.add_parser('system')
+    parser_system = subparsers.add_parser('system', help='Commands for system management')
     system.initialize(parser_system)
 
-    parser_config = subparsers.add_parser('config')
+    parser_config = subparsers.add_parser('config', help='Launches configuration manager')
     parser_config.set_defaults(func=config.run, action='run')
 
     argcomplete.autocomplete(parser)
